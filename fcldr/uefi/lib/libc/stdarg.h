adaedbafc79cb978d718dded7260a882b0b648f8
Jozef Nagy <5c57bfe6@proton.me>
2023-04-07 11:03:45 +0200

    [fcldr/uefi]: Started rewriting the UEFI bootloader

---
+++ fcldr/uefi/lib/libc/stdarg.h
@@ -0,0 +1,11 @@
+#ifndef FCLDR_UEFI_BOOT_STDARG_H
+#define FCLDR_UEFI_BOOT_STDARG_H
+
+typedef __builtin_va_list va_list;
+
+#define va_start(v,l)	    __builtin_va_start(v,l)
+#define va_end(v)	        __builtin_va_end(v)
+#define va_arg(v,l)	        __builtin_va_arg(v,l)
+#define va_copy(d,s)	    __builtin_va_copy(d,s)
+
+#endif // FCLDR_UEFI_BOOT_STDARG_H
---
+++ fcldr/uefi/lib/libc/stdbool.h
@@ -0,0 +1,8 @@
+#ifndef FCLDR_UEFI_BOOT_STDBOOL_H
+#define FCLDR_UEFI_BOOT_STDBOOL_H
+
+typedef _Bool bool;
+#define true    ((bool)1)
+#define false   ((bool)0)
+
+#endif // FCLDR_UEFI_BOOT_STDBOOL_H
