5029c88876e02be12290c539e771d597a6a6e048
Jozef Nagy <5c57bfe6@proton.me>
2023-04-07 22:28:06 +0200

    [fckrnl/dd]: Fixed COM driver

--- fckrnl/dd/com/com.h
+++ fckrnl/dd/com/com.h
@@ -15,4 +15,8 @@ void com_send_char(com_port_t port, char c);
 void com_send_string(com_port_t port, char *str);
 void com_recv(com_port_t port);
 
+///
+bool com_is_transmit_empty(com_port_t port);
+bool com_is_received(com_port_t port);
+
 #endif /* __COM_H_ */
