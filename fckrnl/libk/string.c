241dae393849e582403e958b4300676e0fec65d6
Jozef Nagy <5c57bfe6@proton.me>
2023-04-09 18:18:37 +0200

    oh yeah i added memcmp() like a boss

--- fckrnl/libk/string.c
+++ fckrnl/libk/string.c
@@ -10,3 +10,16 @@ void *memset(void *ptr, uint32_t val, size_t n)
 
 	return ptr;
 }
+
+int memcmp(const void *p1, const void *p2, size_t n)
+{
+	const unsigned char *ptr1 = p1;
+	const unsigned char *ptr2 = p2;
+
+	for (size_t i = 0; i < n; i++, ptr1++, ptr2++) {
+		if (*ptr1 != *ptr2)
+			return (*ptr1 - *ptr2);
+	}
+
+	return 0;
+}
--- fckrnl/libk/string.h
+++ fckrnl/libk/string.h
@@ -5,5 +5,6 @@
 #include <stdint.h>
 
 void *memset(void *ptr, uint32_t val, size_t n);
+int memcmp(const void *p1, const void *p2, size_t n);
 
 #endif /* __STRING_H_ */
